3b099036e31c3af3936764c54718ce1ce39a1b2c
Philip Panyukov <ppanyukov@googlemail.com>
2022-05-17 16:16:15 +0100

    Setup GitHub actions CI build and fix build in Linux (#2)
    * Create makefile.yml
    
    * fix docker build on Linux
    
    * build: dump env vars
    
    * build: upgrade linter v1.26.0 => v1.46.2
    
    * build: more fixing for linux and github actions

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
 # Runs docker container.
 # Main purpose is to use on Windows but can be used by all, especially to avoid
@@ -44,6 +44,22 @@ echo "Running in docker container ${DOCKER_IMAGE}..."
         USER_FLAG="-u $(id -u):$(id -g)"
     fi
 
+
+    # Another annoying Linux issue: failed to initialize build cache at /.cache/go-build
+    # This is because we don't have HOME defined for Linux.
+    #
+    # Completely disable if running in GitHub actions CI because we still get error
+    # mkdir /home/runner/.cache/go-build/00: permission denied
+    #
+    # See:
+    #   - https://forum.golangbridge.org/t/go-build-failed/19551
+    #   - https://github.com/openshift/release/issues/9748
+    Z_GOCACHE="$(go env GOCACHE)"
+    if test "true" == "${GITHUB_ACTIONS}"
+    then
+      Z_GOCACHE="/tmp"
+    fi
+
     set -x
     docker run \
       -i \
@@ -51,10 +67,13 @@ echo "Running in docker container ${DOCKER_IMAGE}..."
       ${USER_FLAG} \
       --rm \
       -v "${PWD}:${PWD}":cached \
+      -v "${Z_GOCACHE}":"${Z_GOCACHE}":cached \
       -w ${PWD} \
+      -e GOCACHE="${Z_GOCACHE}" \
       -e Z_GOOS="${Z_GOOS}" \
       -e Z_GOARCH="${Z_GOARCH}" \
       -e Z_GOBIN="${PWD}/bin_tools/linux_amd64" \
+      -e GOLANGCI_LINT_CACHE=/tmp \
       ${DOCKER_IMAGE} \
       "$@"
   )
