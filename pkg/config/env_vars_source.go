9be477ca04fb372b9245338662ea02e3d9814f21
Philip Panyukov <ppanyukov@googlemail.com>
2023-01-21 15:35:35 +0000

    Fix returning private types (#8)
    Not sure why it worked but it did, but that's probably not a good idea :)

--- pkg/config/env_vars_source.go
+++ pkg/config/env_vars_source.go
@@ -8,15 +8,15 @@ import (
 
 // NewEnvVarsSource creates configuration source from environmental variables
 // in the same way ASP.NET does.
-func NewEnvVarsSource(prefix string) *envVarsSource {
+func NewEnvVarsSource(prefix string) *EnvVarsSource {
 	envVars := make(map[string]string)
 	for _, val := range os.Environ() {
 		fields := strings.SplitN(val, "=", 2)
 		envVars[fields[0]] = fields[1]
 	}
 
-	return &envVarsSource{
-		name:   fmt.Sprintf("envVarsSource Prefix: '%s'", prefix),
+	return &EnvVarsSource{
+		name:   fmt.Sprintf("EnvVarsSource Prefix: '%s'", prefix),
 		prefix: prefix,
 		m:      envVars,
 	}
@@ -24,32 +24,32 @@ func NewEnvVarsSource(prefix string) *envVarsSource {
 
 // NewEnvVarsMapSource is same as NewEnvVarsSource except the
 // values come from the user-supplied map.
-func NewEnvVarsMapSource(prefix string, m map[string]string) *envVarsSource {
-	return &envVarsSource{
+func NewEnvVarsMapSource(prefix string, m map[string]string) *EnvVarsSource {
+	return &EnvVarsSource{
 		name:   fmt.Sprintf("envVarsMapSource Prefix: '%s'", prefix),
 		prefix: prefix,
 		m:      m,
 	}
 }
 
-type envVarsSource struct {
+type EnvVarsSource struct {
 	name   string
 	prefix string
 	m      map[string]string
 }
 
 // WithName sets the name of this source and returns itself.
-func (s *envVarsSource) WithName(name string) *envVarsSource {
+func (s *EnvVarsSource) WithName(name string) *EnvVarsSource {
 	s.name = name
 	return s
 }
 
 // Name is the name of this source. Part of [config.Source] interface.
-func (s *envVarsSource) Name() string {
+func (s *EnvVarsSource) Name() string {
 	return s.name
 }
 
-func (s *envVarsSource) Build() (Config, error) {
+func (s *EnvVarsSource) Build() (Config, error) {
 	m := newEnvVarsLoader(s.prefix).Load(s.m)
 	return newConfigImpl(s, m), nil
 }
--- pkg/config/json_source.go
+++ pkg/config/json_source.go
@@ -7,37 +7,37 @@ import (
 )
 
 // NewJsonSource creates configuration source for Json which implements [config.Source].
-func NewJsonSource(json []byte) *jsonSource {
-	return &jsonSource{
+func NewJsonSource(json []byte) *JsonSource {
+	return &JsonSource{
 		json: json,
-		name: "jsonSource",
+		name: "JsonSource",
 	}
 }
 
-// jsonSource implements [config.Source] interface.
-type jsonSource struct {
+// JsonSource implements [config.Source] interface.
+type JsonSource struct {
 	json []byte
 	name string
 }
 
 // WithName sets the name of this source and returns itself.
-func (s *jsonSource) WithName(name string) *jsonSource {
+func (s *JsonSource) WithName(name string) *JsonSource {
 	s.name = name
 	return s
 }
 
 // Name is the name of this source. Part of [config.Source] interface.
-func (s *jsonSource) Name() string {
+func (s *JsonSource) Name() string {
 	return s.name
 }
 
 // Build builds Config. Part of [config.Source] interface.
-func (s *jsonSource) Build() (Config, error) {
+func (s *JsonSource) Build() (Config, error) {
 	parser := newJsonLoader()
 	r := bytes.NewBuffer(s.json)
 	m, err := parser.Load(r)
 	if err != nil {
-		return nil, errors.Errorf("jsonSource: %s: %v", s.name, err)
+		return nil, errors.Errorf("JsonSource: %s: %v", s.name, err)
 	}
 
 	return newConfigImpl(s, m), nil
